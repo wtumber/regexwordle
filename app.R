8c8511bbb35c7ae20df0051f8f5bc74f968da016
wtumber <tumberw@gmail.com>
2023-04-30 11:16:09 +0100

    add button

--- app.R
+++ app.R
@@ -22,7 +22,8 @@ ui <- fluidPage(
     # Sidebar with a slider input for number of bins 
     sidebarLayout(
         sidebarPanel(
-            textInput("regex_to_match",label = "Input regex",placeholder = ".....")
+            textInput("regex_to_match",label = "Input regex",placeholder = "....."),
+            shiny::actionButton("send","Submit")
         ),
 
         # Show a plot of the generated distribution
@@ -34,21 +35,29 @@ ui <- fluidPage(
 
 # Define server logic required to draw a histogram
 server <- function(input, output) {
+  
+  
+  df <- eventReactive(input$send, {
+    
+    
+    does_word_match_regex <- sapply(word_list$words, function(x) grepl(input$regex_to_match,x,ignore.case=TRUE,perl=TRUE) )
+    
+    matching_letter_matrix <- data.frame(do.call(rbind, strsplit(word_list[does_word_match_regex,1],split = "")))
+    
+    df <- matching_letter_matrix %>% 
+      tidyr::gather() %>% 
+      dplyr::count(key, value, name = "n") %>% 
+      dplyr::arrange(key,desc(n))%>%
+      dplyr::group_by(key) %>%
+      dplyr::slice_max(n, n = 10) 
+    
+  })
+  
+  
 
     output$results <- renderPlot({
-      does_word_match_regex <- sapply(word_list$words, function(x) grepl(input$regex_to_match,x,ignore.case=TRUE,perl=TRUE) )
       
-      matching_letter_matrix <- data.frame(do.call(rbind, strsplit(word_list[does_word_match_regex,1],split = "")))
-      
-      df <- matching_letter_matrix %>% 
-        tidyr::gather() %>% 
-        dplyr::count(key, value, name = "n") %>% 
-        dplyr::arrange(key,desc(n))%>%
-        dplyr::group_by(key) %>%
-        dplyr::slice_max(n, n = 10) 
-      
-      
-      ggplot2::ggplot(df, ggplot2::aes(x = n, y = value)) + 
+      ggplot2::ggplot(df(), ggplot2::aes(x = n, y = value)) + 
         ggplot2::geom_bar(stat = "identity") + 
         ggplot2::facet_wrap(~ key , scales = "free",ncol=5) + 
         ggplot2::labs(x = "Count", y = "Value") + 
