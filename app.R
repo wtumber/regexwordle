696ef316d959286b65c94d397067fbb3c269f452
wtumber <tumberw@gmail.com>
2024-05-05 12:25:46 +0100

    Update app UI and functionality for daily use

--- app.R
+++ app.R
@@ -22,13 +22,17 @@ ui <- fluidPage(
     # Sidebar with a slider input for number of bins 
     sidebarLayout(
         sidebarPanel(
-            textInput("regex_to_match",label = "Input regex",placeholder = "....."),
+          
+          textInput("regex_to_match",label = "Letter placement...",placeholder = "....."),
+          textInput("regex_contains",label = "Word contains letters",placeholder = ""),
+          textInput("regex_does_not_contain",label = "Word does not contain letters",placeholder = ""),
             shiny::actionButton("send","Submit")
         ),
 
         # Show a plot of the generated distribution
         mainPanel(
-           plotOutput("results")
+          plotOutput("results"),
+          textOutput("words")
         )
     )
 )
@@ -36,13 +40,61 @@ ui <- fluidPage(
 # Define server logic required to draw a histogram
 server <- function(input, output) {
   
+  processed_regex <-  eventReactive(input$send, {
+    full_regex <- paste0(input$regex_to_match)
+    
+    if(nchar(full_regex) > 0){
+      if(strsplit(full_regex,"")[nchar(full_regex)] != "$"){
+        full_regex <- paste0(full_regex,"$")
+      }
+      
+      if(grepl("…",full_regex,fixed=TRUE)){
+        full_regex <- gsub("…","...",full_regex)
+      }
+    }
+    
+    if(nchar(input$regex_contains) > 1){
+      
+      must_contain_regex <- sapply(strsplit(input$regex_contains,""), function(x){
+        return(paste0("(?=.*",x,")"))
+      })
+      
+      must_contain_regex <- paste0(must_contain_regex,collapse="")
+      
+      full_regex <- paste0(c(must_contain_regex,full_regex),collapse="")
+      
+      
+    }
+    
+    if(nchar(input$regex_does_not_contain) > 1){
+      
+      full_regex <- paste0(c(paste0("(?!.*[",input$regex_does_not_contain,"])"),full_regex),collapse="")
+      
+      
+    }
+    
+    print(full_regex)
+    
+    full_regex
+    
+    
+  })
   
-  df <- eventReactive(input$send, {
+  
+  valid_words <- reactive({
+    
+    full_regex <- processed_regex()
+    
+    does_word_match_regex <- sapply(word_list$words, function(x) grepl(full_regex,x,ignore.case=TRUE,perl=TRUE) )
     
     
-    does_word_match_regex <- sapply(word_list$words, function(x) grepl(input$regex_to_match,x,ignore.case=TRUE,perl=TRUE) )
+    word_list[does_word_match_regex,1]
+  })
+  
+  
+  df <- reactive({
     
-    matching_letter_matrix <- data.frame(do.call(rbind, strsplit(word_list[does_word_match_regex,1],split = "")))
+    matching_letter_matrix <- data.frame(do.call(rbind, strsplit(valid_words(),split = "")))
     
     df <- matching_letter_matrix %>% 
       tidyr::gather() %>% 
@@ -54,16 +106,19 @@ server <- function(input, output) {
   })
   
   
-
     output$results <- renderPlot({
       
       ggplot2::ggplot(df(), ggplot2::aes(x = n, y = value)) + 
         ggplot2::geom_bar(stat = "identity") + 
         ggplot2::facet_wrap(~ key , scales = "free",ncol=5) + 
-        ggplot2::labs(x = "Count", y = "Value") + 
+        ggplot2::labs(x = "Count", y = "Value",title = processed_regex()) + 
         ggplot2::theme_bw()
       
       
+    })
+    
+    output$words <- renderText({
+      valid_words()
     })
 }
 
